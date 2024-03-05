af8d62ecb0fb0ef8b558957ecca18950d4bc96d7
MarcusLehmkuhl <marcusfilipi.ventura@outlook.com>
2024-03-05 15:14:29 -0300

    Remove contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'luiz Otávio',
+    })
 
 
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
